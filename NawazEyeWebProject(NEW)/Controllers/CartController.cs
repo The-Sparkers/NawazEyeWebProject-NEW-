135a51c4786e5e050e00d3bb01343f765c2df51f
Umair Tahir <professor.ut.786@gmail.com>
2018-07-12 19:57:04 +0500

    cartcontroller added

---
+++ NawazEyeWebProject(NEW)/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NawazEyeWebProject_NEW_.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
